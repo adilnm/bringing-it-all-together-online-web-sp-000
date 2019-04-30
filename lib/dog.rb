d6ee390710eb8af10e93bc8bb5c712ad785a4aa4
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-30 09:10:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,8 +52,8 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ? ", hash[name] )
     if !dog.empty?
       new_dog=Dog.new(id:dog[0][0],name:dog[0][1],breed:dog[0][2])
-    else
-      self.create(hash)
+    # else
+    #   self.create(hash)
     end
   end 
   
